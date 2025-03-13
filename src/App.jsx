06a211ee1ab7a2691d932f9ab092a4afb869e564
omar tarik yehya <khalaf.omar20@gmail.com>
2025-03-13 09:28:25 +0200

    feat: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from "./components/Hero.jsx";
 
 
 const App = () => {
     return (
-        <main>
-            <h1 className="text-5xl text-orange-500 font-bold font-circular-web">Welcome to Awwwards</h1>
+        <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Hero/>
+            <section className="z-0 min-h-screen bg-blue-500"/>
         </main>
     )
 }
